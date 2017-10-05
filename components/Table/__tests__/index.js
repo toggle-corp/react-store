25f9476a6c9e7db682f761ff60b2575b3ea87af1
pprabesh <prabes.pathak@gmail.com>
2017-10-05 12:16:09 +0545

    Add test for table  when headers are  added/removed

--- components/Table/__tests__/index.js
+++ components/Table/__tests__/index.js
@@ -25,6 +25,121 @@ describe('Table', () => {
         expect(wrapper.length).toEqual(1);
     });
 });
+describe('Table', () => {
+    const tableHeaders = [
+        { key: 'a', label: 'atest', order: 1 },
+        { key: 'b', label: 'btest', order: 2 },
+        { key: 'c', label: 'ctest', order: 3 },
+
+    ];
+    const tableData = [
+        { a: 5, b: 67 },
+        { a: 44, b: 823 },
+        { a: 44, b: 823 },
+        { a: 34, b: 1 },
+    ];
+
+    const wrapper = shallow(
+        <Table
+            data={tableData}
+            headers={tableHeaders}
+        />,
+    );
+
+    it('renders properly when new header added', () => {
+        expect(wrapper.length).toEqual(1);
+    });
+});
+describe('Table Header', () => {
+    const tableHeaders = [
+        {
+            key: 'a',
+            label: 'atest',
+            order: 1,
+            sortable: true,
+            comparator: (a, b) => a.a - b.a,
+        },
+        {
+            key: 'b',
+            label: 'btest',
+            order: 2,
+            sortable: true,
+            comparator: (b, a) => a.b - b.b,
+        },
+        {
+            key: 'c',
+            label: 'ctest',
+            order: 3,
+        },
+    ];
+
+
+    const tableData = [
+        { a: 5, b: 67, c: 'ok' },
+        { a: 44, b: 13, c: 'ok' },
+        { a: 6, b: 29, c: 'ok' },
+        { a: 84, b: 21, c: 'ok' },
+    ];
+
+    const ascDataForA = [
+        { a: 5, b: 67, c: 'ok' },
+        { a: 6, b: 29, c: 'ok' },
+        { a: 44, b: 13, c: 'ok' },
+        { a: 84, b: 21, c: 'ok' },
+    ];
+
+    const dscDataForA = [
+        { a: 84, b: 21, c: 'ok' },
+        { a: 44, b: 13, c: 'ok' },
+        { a: 6, b: 29, c: 'ok' },
+        { a: 5, b: 67, c: 'ok' },
+    ];
+    const ascDataForB = [
+        { a: 44, b: 13, c: 'ok' },
+        { a: 84, b: 21, c: 'ok' },
+        { a: 6, b: 29, c: 'ok' },
+        { a: 5, b: 67, c: 'ok' },
+    ];
+
+    const dscDataForB = [
+        { a: 5, b: 67, c: 'ok' },
+        { a: 6, b: 29, c: 'ok' },
+        { a: 84, b: 21, c: 'ok' },
+        { a: 44, b: 13, c: 'ok' },
+    ];
+
+    const wrapper = mount(
+        <Table
+            data={tableData}
+            headers={tableHeaders}
+        />,
+    );
+
+    it('sorts properly for header a', () => {
+        // console.log(wrapper.state('data'));
+        const thead = wrapper.find('thead');
+        const th = thead.find('th');
+        th.at(0).simulate('click');
+        // console.log(wrapper.state('data'));
+        expect(wrapper.state('data')).toEqual(dscDataForA);
+        th.at(0).simulate('click');
+        // console.log(wrapper.state('data'));
+        expect(wrapper.state('data')).toEqual(ascDataForA);
+    });
+
+
+    it('sorts properly for header b', () => {
+        // console.log(wrapper.state('data'));
+        const thead = wrapper.find('thead');
+        const th = thead.find('th');
+        th.at(1).simulate('click');
+        // console.log(wrapper.state('data'));
+        expect(wrapper.state('data')).toEqual(dscDataForB);
+        th.at(1).simulate('click');
+        // console.log(wrapper.state('data'));
+        expect(wrapper.state('data')).toEqual(ascDataForB);
+    });
+});
 
 describe('Table Header', () => {
     const tableHeaders = [
@@ -39,27 +154,49 @@ describe('Table Header', () => {
             key: 'b',
             label: 'btest',
             order: 2,
+            sortable: true,
+            comparator: (b, a) => a.b - b.b,
+        },
+        {
+            key: 'c',
+            label: 'ctest',
+            order: 3,
         },
     ];
+
+
     const tableData = [
-        { a: 5, b: 67 },
-        { a: 44, b: 13 },
-        { a: 6, b: 29 },
-        { a: 84, b: 21 },
+        { a: 5, b: 67, c: 'ok' },
+        { a: 44, b: 13, c: 'ok' },
+        { a: 6, b: 29, c: 'ok' },
+        { a: 84, b: 21, c: 'ok' },
     ];
 
-    const ascData = [
-        { a: 5, b: 67 },
-        { a: 6, b: 29 },
-        { a: 44, b: 13 },
-        { a: 84, b: 21 },
+    const ascDataForA = [
+        { a: 5, b: 67, c: 'ok' },
+        { a: 6, b: 29, c: 'ok' },
+        { a: 44, b: 13, c: 'ok' },
+        { a: 84, b: 21, c: 'ok' },
     ];
 
-    const dscData = [
-        { a: 84, b: 21 },
-        { a: 44, b: 13 },
-        { a: 6, b: 29 },
-        { a: 5, b: 67 },
+    const dscDataForA = [
+        { a: 84, b: 21, c: 'ok' },
+        { a: 44, b: 13, c: 'ok' },
+        { a: 6, b: 29, c: 'ok' },
+        { a: 5, b: 67, c: 'ok' },
+    ];
+    const ascDataForB = [
+        { a: 44, b: 13, c: 'ok' },
+        { a: 84, b: 21, c: 'ok' },
+        { a: 6, b: 29, c: 'ok' },
+        { a: 5, b: 67, c: 'ok' },
+    ];
+
+    const dscDataForB = [
+        { a: 5, b: 67, c: 'ok' },
+        { a: 6, b: 29, c: 'ok' },
+        { a: 84, b: 21, c: 'ok' },
+        { a: 44, b: 13, c: 'ok' },
     ];
 
     const wrapper = mount(
@@ -69,15 +206,21 @@ describe('Table Header', () => {
         />,
     );
 
-    it('sorts properly', () => {
+    it('sorts properly when first header is removed', () => {
         console.log(wrapper.state('data'));
+        const newHeaders = tableHeaders.slice(1, tableHeaders.length);
+        // console.log(newHeaders);
+        wrapper.setProps({
+            data: tableData,
+            headers: newHeaders,
+        });
         const thead = wrapper.find('thead');
         const th = thead.find('th');
-        th.first().simulate('click');
+        th.at(1).simulate('click');
         console.log(wrapper.state('data'));
-        expect(wrapper.state('data')).toEqual(dscData);
-        th.first().simulate('click');
+        expect(wrapper.state('data')).toEqual(dscDataForB);
+        th.at(1).simulate('click');
         console.log(wrapper.state('data'));
-        expect(wrapper.state('data')).toEqual(ascData);
+        // expect(wrapper.state('data')).toEqual(ascDataForB);
     });
 });
