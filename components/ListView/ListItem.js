afeff9af3c1e00cdfc5f95af4402a5f1e256010c
AdityaKhatri <adityakhatri47@gmail.com>
2017-10-28 11:09:03 +0545

    Remove predefined style for list item

--- components/ListView/ListItem.js
+++ components/ListView/ListItem.js
@@ -25,19 +25,10 @@ export default class ListItem extends React.PureComponent {
     static propTypes = propTypes;
     static defaultProps = defaultProps;
 
-    constructor(props) {
-        super(props);
-
-        console.log('Create listitem');
-    }
-
     render() {
-        console.log('Rendering listitem');
-
         return (
             <div
                 className={this.props.className}
-                styleName="list-item"
             >
                 { this.props.children }
             </div>
