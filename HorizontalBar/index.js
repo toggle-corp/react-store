5aa9579a154cf9854180320520c37d46574aa1eb
Sameer Rai <sameerai2736@gmail.com>
2017-11-13 17:01:20 +0545

    Add ResponsiveHOC to HorizontalBar and separate logic from data.

--- HorizontalBar/index.js
+++ HorizontalBar/index.js
@@ -5,82 +5,68 @@ import { scaleLinear, scaleBand } from 'd3-scale';
 import { axisLeft, axisBottom } from 'd3-axis';
 import { max } from 'd3-array';
 import { PropTypes } from 'prop-types';
+import Responsive from '../Responsive';
 import styles from './styles.scss';
 
 const propTypes = {
-    className: PropTypes.string,
+    boundingClientRect: PropTypes.shape({
+        width: PropTypes.number,
+        height: PropTypes.number,
+    }).isRequired,
+    data: PropTypes.arrayOf(PropTypes.object),
+    valueAccessor: PropTypes.func.isRequired,
+    labelAccessor: PropTypes.func.isRequired,
+    margins: PropTypes.shape({
+        top: PropTypes.number,
+        right: PropTypes.number,
+        bottom: PropTypes.number,
+        left: PropTypes.number,
+    }),
 };
 
 const defaultProps = {
-    className: '',
+    data: [],
+    margins: {
+        top: 50,
+        right: 50,
+        bottom: 100,
+        left: 100,
+    },
 };
 
+@Responsive
 @CSSModules(styles)
 export default class HorizontalBar extends React.PureComponent {
     static propTypes = propTypes;
     static defaultProps = defaultProps;
 
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            boundingClientRect: {},
-            render: false,
-        };
-    }
-
     componentDidMount() {
-        window.addEventListener('resize', this.handleResize);
-
-        setTimeout(() => {
-            this.setState({
-                render: true,
-                boundingClientRect: this.container.getBoundingClientRect(),
-            });
-        }, 0);
+        this.renderChart();
     }
 
     componentDidUpdate() {
         this.renderChart();
     }
-    componentWillUnmount() {
-        window.removeEventListener('resize', this.handleResize());
-    }
-
-    handleResize = () => {
-        this.setState({
-            render: true,
-            boundingClientRect: this.container.getBoundingClientRect(),
-        });
-    }
 
     renderChart() {
-        if (!this.state.render) {
+        const {
+            data,
+            boundingClientRect,
+            valueAccessor,
+            labelAccessor,
+            margins,
+        } = this.props;
+
+        if (!boundingClientRect.width) {
             return;
         }
-
-        const data = [
-            { label: 'Category 1', value: 19 },
-            { label: 'Category 2', value: 5 },
-            { label: 'Category 3', value: 13 },
-            { label: 'Category 4', value: 17 },
-            { label: 'Category 5', value: 19 },
-            { label: 'Category 6', value: 27 },
-        ];
-
+        let { width, height } = boundingClientRect;
         const {
             top,
             right,
             bottom,
             left,
-        } = {
-            top: 50,
-            right: 50,
-            bottom: 50,
-            left: 100,
-        };
-
-        let { width, height } = this.state.boundingClientRect;
+        } = margins;
 
         width = width - left - right;
         height = height - top - bottom;
@@ -99,11 +85,11 @@ export default class HorizontalBar extends React.PureComponent {
 
         const x = scaleLinear()
             .range([0, width])
-            .domain([0, max(data, d => d.value)]);
+            .domain([0, max(data, d => valueAccessor(d))]);
 
         const y = scaleBand()
             .rangeRound([height, 0])
-            .domain(data.map(d => d.label))
+            .domain(data.map(d => labelAccessor(d)))
             .padding(0.2);
 
         const xAxis = axisBottom(x);
@@ -127,23 +113,17 @@ export default class HorizontalBar extends React.PureComponent {
             .append('rect')
             .attr('class', 'bar')
             .attr('x', 0)
-            .attr('y', d => y(d.label))
+            .attr('y', d => y(labelAccessor(d)))
             .attr('height', y.bandwidth())
-            .attr('width', d => x(d.value));
+            .attr('width', d => x(valueAccessor(d)));
     }
 
     render() {
         return (
-            <div
-                className={this.props.className}
-                styleName="horizontal-bar-container"
-                ref={(el) => { this.container = el; }}
-            >
-                <svg
-                    className="svg"
-                    ref={(elem) => { this.svg = elem; }}
-                />
-            </div>
+            <svg
+                className="horizontalbar"
+                ref={(elem) => { this.svg = elem; }}
+            />
         );
     }
 }
