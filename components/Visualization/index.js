38381ea0f93ccc3270ac571a9fe6f5c180b6fcc1
tnagorra <weathermist@gmail.com>
2017-11-20 21:31:46 +0545

    Comment out unused visualizations

--- components/Visualization/index.js
+++ components/Visualization/index.js
@@ -2,7 +2,8 @@
  * @author tnagorra <weathermist@gmail.com>
  */
 
+// eslint-disable-next-line import/prefer-default-export
 export { default as BarChart } from './BarChart';
-export { default as DonutChart } from './DonutChart';
-export { default as MountainChart } from './MountainChart';
+// export { default as DonutChart } from './DonutChart';
+// export { default as MountainChart } from './MountainChart';
 export { default as TimeSeries } from './TimeSeries';
