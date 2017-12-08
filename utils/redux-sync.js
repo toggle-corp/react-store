8040c94ed4a0695c4757dca182f9a8c7c36a15aa
Bibek Dahal <bibekdahal.bd16@gmail.com>
2017-12-08 16:20:03 +0545

    Update redux-sync for persistor and localforage

--- utils/redux-sync.js
+++ utils/redux-sync.js
@@ -1,25 +1,50 @@
-import { KEY_PREFIX } from 'redux-persist';
+import localForage from 'localforage';
+import * as lfo from 'localforage-observable';
+import { REHYDRATE } from 'redux-persist';
+import Observable from 'zen-observable';
+
+const localforage = lfo.extendPrototype(localForage);
+localforage.newObservable.factory = subscribeFn => new Observable(subscribeFn);
 
 
-// Listen to localStorage changes and rehydrate the persistor
+// Listen to localforage changes and rehydrate the store
 // with new data.
 // Allows blacklisted keys to be ignored while syncing.
-const reduxSync = (persistor, blacklist = undefined, keyPrefixOverride = undefined) => {
-    const handleStorageEvent = (event) => {
-        const keyPrefix = keyPrefixOverride || KEY_PREFIX;
+const reduxSync = (store, persistor, blacklist = undefined, key = undefined) => {
+    localforage.ready(() => {
+        localforage.configObservables({
+            crossTabNotification: true,
+        });
+        const observable = localforage.newObservable({
+            crossTabNotification: true,
+            changeDetection: false,
+        });
 
-        if (event.key && event.key.indexOf(keyPrefix) === 0) {
-            const keyspace = event.key.substr(keyPrefix.length);
-            if (blacklist && blacklist.indexOf(keyspace) !== -1) {
-                return;
-            }
-
-            const partialState = {};
-            partialState[keyspace] = event.newValue;
-            persistor.rehydrate(partialState, { serial: true });
-        }
-    };
-    window.addEventListener('storage', handleStorageEvent, false);
+        observable.subscribe({
+            next: (args) => {
+                if (args.crossTabNotification) {
+                    const stringData = JSON.parse(args.newValue);
+                    const data = {};
+                    Object.keys(stringData).forEach((k) => {
+                        if (blacklist && blacklist.indexOf(k) !== -1) {
+                            return;
+                        }
+                        data[k] = JSON.parse(stringData[k]);
+                    });
+                    const rehydrateAction = {
+                        type: REHYDRATE,
+                        payload: data,
+                        key,
+                    };
+                    store.dispatch(rehydrateAction);
+                    persistor.dispatch(rehydrateAction);
+                }
+            },
+            error: (err) => {
+                console.error('Found an error while observing local-forage', err);
+            },
+        });
+    });
 };
 
 export default reduxSync;
