a851b971539b49d25c4e559fbc68ffb46da780e1
Sameer Rai <sameerai2736@gmail.com>
2017-12-12 16:41:46 +0545

    Add selction of data classes

--- SunBurstView/index.js
+++ SunBurstView/index.js
@@ -2,6 +2,7 @@ import React, { PureComponent } from 'react';
 import CSSModules from 'react-css-modules';
 import { PropTypes } from 'prop-types';
 import { Button, Dropdown } from 'semantic-ui-react';
+import { range } from 'd3-array';
 import { categoricalColorNames, getCategoryColorScheme } from '../../ColorScheme';
 import SunBurst from '../SunBurst';
 import styles from './styles.scss';
@@ -23,19 +24,35 @@ export default class SunBurstView extends PureComponent {
 
     constructor(props) {
         super(props);
-        this.state = { colorScheme: undefined };
+        this.state = {
+            colorScheme: undefined,
+            effectiveColorScheme: undefined,
+        };
+        this.categories = [];
         this.colors = categoricalColorNames()
             .map(name => ({ text: name, value: name }));
     }
 
     componentWillReceiveProps(newProps) {
-        this.setState({ colorScheme: newProps.colorScheme });
+        this.setState({
+            colorScheme: newProps.colorScheme,
+            effectiveColorScheme: newProps.colorScheme,
+        });
     }
 
     handleSelection = (e, data) => {
         const colors = getCategoryColorScheme(data.value);
+        this.categories = range(1, colors.length + 1)
+            .map(item => ({ text: item, value: item }));
         this.setState({
             colorScheme: colors,
+            effectiveColorScheme: colors,
+        });
+    }
+
+    handleNoOfCategories = (e, data) => {
+        this.setState({
+            effectiveColorScheme: this.state.colorScheme.slice(0, data.value),
         });
     }
 
@@ -44,7 +61,7 @@ export default class SunBurstView extends PureComponent {
     }
 
     handleReset = () => {
-        this.chart.wrappedComponent.renderChart();
+        this.render();
     }
 
     render() {
@@ -72,12 +89,19 @@ export default class SunBurstView extends PureComponent {
                         placeholder="ColorScheme"
                         onChange={this.handleSelection}
                     />
+                    <Dropdown
+                        options={this.categories}
+                        openOnFocus
+                        selection
+                        placeholder="No of Data Classes"
+                        onChange={this.handleNoOfCategories}
+                    />
                 </div>
                 <SunBurst
                     styleName="sunburst"
                     ref={(instance) => { this.chart = instance; }}
                     {...otherProps}
-                    colorScheme={this.state.colorScheme}
+                    colorScheme={this.state.effectiveColorScheme}
                 />
             </div>
         );
