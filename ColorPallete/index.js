db04944e36a42c7a36ee8d2f21509acae3bdda7b
Sameer Rai <sameerai2736@gmail.com>
2017-12-14 13:01:41 +0545

    Add ColorPallete component

---
+++ ColorPallete/index.js
@@ -0,0 +1,51 @@
+import React, { PureComponent } from 'react';
+import { PropTypes } from 'prop-types';
+
+const propTypes = {
+    colorScheme: PropTypes.arrayOf(PropTypes.string),
+    width: PropTypes.number,
+    height: PropTypes.number,
+    align: PropTypes.oneOf(['vertical', 'horizontal']),
+};
+
+const defaultProps = {
+    colorScheme: [],
+    width: 20,
+    height: 20,
+    align: 'horizontal',
+};
+
+export default class ColorPallete extends PureComponent {
+    static propTypes = propTypes;
+    static defaultProps = defaultProps;
+
+    render() {
+        const {
+            colorScheme,
+            width,
+            height,
+            align,
+        } = this.props;
+
+        const float = (align === 'horizontal' ? 'left' : '');
+        const pallete = colorScheme.map(color =>
+            (
+                <span
+                    key={`pallete-${color}`}
+                    className="swatch"
+                    style={{
+                        backgroundColor: color,
+                        display: 'block',
+                        width: `${width}px`,
+                        height: `${height}px`,
+                        float: `${float}`,
+                    }}
+                />
+            ));
+        return (
+            <div>
+                { pallete }
+            </div>
+        );
+    }
+}
