645eb72d3a85da1267bd82ee53bc35bfd5a447e3
AdityaKhatri <adityakhatri47@gmail.com>
2017-12-21 12:16:15 +0545

    Add function to return text wrt bgcolor

--- utils/common.js
+++ utils/common.js
@@ -258,3 +258,12 @@ export const reverseRoute = (route, params) => {
 
     return paths.join('/');
 };
+
+export const getContrastYIQ = (hexcolor) => {
+    const r = parseInt(hexcolor.substr(1, 2), 16);
+    const g = parseInt(hexcolor.substr(3, 2), 16);
+    const b = parseInt(hexcolor.substr(5, 2), 16);
+    const yiq = ((r * 299) + (g * 587) + (b * 114)) / 1000;
+
+    return (yiq >= 128) ? '#212121' : '#fff';
+};
