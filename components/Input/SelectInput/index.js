cfcdd3a488a88992a2bd50c350192725262e399b
tnagorra <weathermist@gmail.com>
2018-01-19 12:20:12 +0545

    Reset option and value on prop change

--- components/Input/SelectInput/index.js
+++ components/Input/SelectInput/index.js
@@ -23,11 +23,23 @@ export default class SelectInput extends React.PureComponent {
     static propTypes = singleSelectInputPropTypes;
     static defaultProps = singleSelectInputDefaultProps;
 
+    static getActiveOptionLabel = (props) => {
+        const {
+            value,
+            labelSelector,
+            keySelector,
+            options,
+        } = props;
+
+        const activeOption = options.find(d => keySelector(d) === value);
+        return activeOption ? labelSelector(activeOption) : '';
+    }
+
     constructor(props) {
         super(props);
 
         this.state = {
-            inputValue: this.getActiveOptionLabel(props),
+            inputValue: SelectInput.getActiveOptionLabel(props),
             displayOptions: props.options,
         };
     }
@@ -49,25 +61,15 @@ export default class SelectInput extends React.PureComponent {
         } = this.props;
 
         if (nextProps.value !== oldValue || nextProps.options !== oldOptions) {
+            // NOTE: filter will be cleared
             this.setState({
-                inputValue: this.getActiveOptionLabel(nextProps),
+                inputValue: SelectInput.getActiveOptionLabel(nextProps),
+                displayOptions: nextProps.options,
             });
         }
     }
 
     getValue = () => this.props.value
-
-    getActiveOptionLabel = (props) => {
-        const {
-            value,
-            labelSelector,
-            keySelector,
-            options,
-        } = props;
-
-        const activeOption = options.find(d => keySelector(d) === value);
-        return (activeOption && labelSelector(activeOption)) || '';
-    }
 
     handleInputChange = (e) => { handleInputValueChange(this, e.target.value); }
 
@@ -85,6 +87,7 @@ export default class SelectInput extends React.PureComponent {
             displayOptions: options,
             inputValue,
         });
+        // XXX: why set these on blur?
     }
 
     handleOptionClick = (key) => {
