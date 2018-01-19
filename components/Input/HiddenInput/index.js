e4b76b3b5ec4c34e16e38822e6c699010468253d
tnagorra <weathermist@gmail.com>
2018-01-19 12:10:46 +0545

    Remove internal state from HiddenInput

--- components/Input/HiddenInput/index.js
+++ components/Input/HiddenInput/index.js
@@ -16,44 +16,25 @@ const propTypes = {
         PropTypes.bool,
         PropTypes.object,
         PropTypes.array,
-    ]),
+    ]).isRequired,
 };
 
 const defaultProps = {
     className: '',
-    value: '',
 };
 
 @CSSModules(styles, { allowMultiple: true })
-export default class TextInput extends React.PureComponent {
+export default class HiddenInput extends React.PureComponent {
     static propTypes = propTypes;
     static defaultProps = defaultProps;
 
-    constructor(props) {
-        super(props);
-
-        const value = this.props.value || '';
-        this.state = {
-            value,
-        };
-    }
-
-    componentWillReceiveProps(nextProps) {
-        this.setState({
-            value: nextProps.value,
-        });
-    }
-
-    getValue = () => this.state.value;
+    getValue = () => this.props.value;
 
     render() {
         const {
             className,
-        } = this.props;
-
-        const {
             value,
-        } = this.state;
+        } = this.props;
 
         return (
             <input
