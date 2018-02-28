a4c6c1a6e9e3ae6f3d195baf1c970d3a9edcccfb
AdityaKhatri <adityakhatri47@gmail.com>
2018-02-28 16:02:00 +0545

    Add select input with list component

---
+++ components/Input/SelectInputWithList/index.js
@@ -0,0 +1,274 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import MultiSelectInput from '../../Input/SelectInput/MultiSelectInput';
+import DangerButton from '../../Action/Button/DangerButton';
+import ListView from '../../View/List/ListView';
+import { iconNames } from '../../../../../constants';
+
+import styles from './styles.scss';
+
+const propTypes = {
+    /**
+     * Key selector function
+     * should return key from provided row data
+     */
+    keySelector: PropTypes.func,
+
+    /**
+     * String to show in case of error
+     */
+    error: PropTypes.string,
+
+    /**
+     * Hint text
+     */
+    hint: PropTypes.string,
+
+    showHintAndError: PropTypes.bool,
+
+    disabled: PropTypes.bool,
+
+    onChange: PropTypes.func,
+
+    /**
+     * Value selector function
+     * should return value from provided row data
+     */
+    labelSelector: PropTypes.func,
+
+    label: PropTypes.oneOfType([
+        PropTypes.string,
+        PropTypes.number,
+    ]),
+
+    value: PropTypes.arrayOf(PropTypes.oneOfType([
+        PropTypes.string,
+        PropTypes.number,
+    ])),
+
+    showRemoveFromListButton: PropTypes.bool,
+
+    className: PropTypes.string,
+    /**
+     * Options to be shown
+     */
+    options: PropTypes.arrayOf(
+        PropTypes.shape({
+            key: PropTypes.oneOfType([
+                PropTypes.string,
+                PropTypes.number,
+            ]),
+            label: PropTypes.string,
+        }),
+    ),
+
+    optionsIdentifier: PropTypes.string,
+};
+
+const defaultProps = {
+    className: '',
+    keySelector: d => (d || {}).key,
+    labelSelector: d => (d || {}).label,
+    error: '',
+    hint: '',
+    onChange: undefined,
+    showHintAndError: true,
+    optionsIdentifier: undefined,
+    options: [],
+    value: [],
+    label: '',
+    showRemoveFromListButton: true,
+    disabled: false,
+};
+
+const emptyList = [];
+
+export default class SelectInputWithList extends React.PureComponent {
+    static propTypes = propTypes;
+    static defaultProps = defaultProps;
+
+    constructor(props) {
+        super(props);
+        const {
+            options,
+            value,
+        } = this.props;
+
+        const objectValues = this.getObjectFromValue(options, value);
+
+        this.state = {
+            objectValues,
+        };
+    }
+
+    getObjectFromValue = (options, value) => (
+        (options || emptyList).filter(d => (
+            value.indexOf(d.key) !== -1
+        ))
+    )
+
+    getClassName = () => {
+        const { className } = this.props;
+        const { error } = this.state;
+
+        const classNames = [
+            className,
+            styles['select-input-with-list'],
+            'select-input-with-list',
+        ];
+
+        if (error) {
+            classNames.push(styles.error);
+            classNames.push('error');
+        }
+
+        return classNames.join(' ');
+    }
+
+    handleSelectInputChange = (values) => {
+        const {
+            onChange,
+            options,
+        } = this.props;
+
+        const objectValues = this.getObjectFromValue(options, values);
+        this.setState({ objectValues });
+
+        if (onChange) {
+            onChange(values);
+        }
+    }
+
+    handleListItemRemove = (key) => {
+        const {
+            onChange,
+            value,
+            options,
+            disabled,
+        } = this.props;
+
+        if (!disabled) {
+            const index = value.indexOf(key);
+            const newValue = [...value];
+
+            if (index !== -1) {
+                newValue.splice(index, 1);
+            }
+
+            const objectValues = this.getObjectFromValue(options, newValue);
+            this.setState({ objectValues });
+
+            if (onChange) {
+                onChange(newValue);
+            }
+        }
+    }
+
+    renderSelectedItem = (key, data) => {
+        const {
+            disabled,
+            showRemoveFromListButton,
+        } = this.props;
+        let additionalStyle = '';
+        if (disabled) {
+            additionalStyle = styles.disabled;
+        }
+
+        return (
+            <div
+                className={styles['selected-item']}
+                key={key}
+            >
+                <span
+                    className={styles['selected-item-label']}
+                >
+                    {this.props.labelSelector(data)}
+                </span>
+                {showRemoveFromListButton &&
+                    <DangerButton
+                        onClick={() => this.handleListItemRemove(key)}
+                        className={`${additionalStyle} ${styles['remove-button']}`}
+                        type="button"
+                        smallHorizontalPadding
+                        smallVerticalPadding
+                        transparent
+                    >
+                        <span className={iconNames.close} />
+                    </DangerButton>
+                }
+            </div>
+        );
+    }
+
+    render() {
+        const {
+            options,
+            value,
+            keySelector,
+            labelSelector,
+            label,
+            optionsIdentifier,
+            error,
+            hint,
+            showHintAndError,
+            disabled,
+            ...otherProps
+        } = this.props;
+
+        const {
+            objectValues,
+        } = this.state;
+
+        return (
+            <div className={this.getClassName()}>
+                <MultiSelectInput
+                    value={value}
+                    label={label}
+                    options={options}
+                    keySelector={keySelector}
+                    labelSelector={labelSelector}
+                    optionsIdentifier={optionsIdentifier}
+                    onChange={this.handleSelectInputChange}
+                    className={`${styles.input} input`}
+                    disabled={disabled}
+                    error={error}
+                    {...otherProps}
+                />
+                <ListView
+                    className={`${styles.list} list`}
+                    data={objectValues}
+                    modifier={this.renderSelectedItem}
+                    keyExtractor={keySelector}
+                />
+                {
+                    showHintAndError && [
+                        !error && hint && (
+                            <p
+                                key="hint"
+                                className={`${styles.hint} hint`}
+                            >
+                                {hint}
+                            </p>
+                        ),
+                        error && !hint && (
+                            <p
+                                key="error"
+                                className={`${styles.error} error`}
+                            >
+                                {error}
+                            </p>
+                        ),
+                        !error && !hint && (
+                            <p
+                                key="empty"
+                                className={`${styles.empty} empty`}
+                            >
+                                -
+                            </p>
+                        ),
+                    ]
+                }
+            </div>
+        );
+    }
+}
