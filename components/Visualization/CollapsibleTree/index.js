94df0000e43726bf7d66a274171da578d9dbd217
Sameer Rai <sameerai2736@gmail.com>
2018-03-05 16:41:06 +0545

    Refactor Collapsible Tree

--- components/Visualization/CollapsibleTree/index.js
+++ components/Visualization/CollapsibleTree/index.js
@@ -9,9 +9,6 @@ import SvgSaver from 'svgsaver';
 import Responsive from '../../General/Responsive';
 import { getStandardFilename, isObjectEmpty } from '../../../utils/common';
 import LoadingAnimation from '../../View/LoadingAnimation';
-
-// FIXME: don't use globals
-// eslint-disable-next-line no-unused-vars
 import styles from './styles.scss';
 
 /**
@@ -67,10 +64,53 @@ export default class CollapsibleTree extends React.PureComponent {
     static defaultProps = defaultProps;
 
     componentDidMount() {
-        this.renderChart();
+        this.drawChart();
     }
     componentDidUpdate() {
-        this.renderChart();
+        this.redrawChart();
+    }
+
+    setContext = (width, height, margins) => {
+        const {
+            top,
+            right,
+            bottom,
+            left,
+        } = margins;
+        return select(this.svg)
+            .attr('width', width + left + right)
+            .attr('height', height + top + bottom)
+            .append('g')
+            .attr('transform', `translate(${left},${top})`);
+    }
+
+    setupChart = () => {
+        const {
+            data,
+            childrenAccessor,
+            boundingClientRect,
+            colorScheme,
+            margins,
+        } = this.props;
+
+        let { width, height } = boundingClientRect;
+        const {
+            top,
+            right,
+            bottom,
+            left,
+        } = margins;
+
+        width = width - left - right;
+        height = height - top - bottom;
+
+        this.trees = tree().size([height, width - 160]);
+        this.root = hierarchy(data, childrenAccessor);
+        this.root.x0 = height / 2;
+        this.root.y0 = 0;
+        this.colors = scaleOrdinal().range(colorScheme);
+        this.context = this.setContext(width, height, margins);
+        this.duration = 0;
     }
 
     save = () => {
@@ -79,14 +119,170 @@ export default class CollapsibleTree extends React.PureComponent {
         svgsaver.asSvg(svg.node(), `${getStandardFilename('collapsible-tree', 'graph')}.svg`);
     }
 
-    renderChart = () => {
+    topicColors = (node) => {
+        const { labelAccessor } = this.props;
+        let color = this.colors(0);
+        if (node.depth === 0 || node.depth === 1) {
+            color = this.colors(labelAccessor(node.data));
+        } else {
+            color = this.topicColors(node.parent);
+        }
+        return color;
+    }
+
+    diagonal = (s, d) => {
+        const path = `M ${s.y} ${s.x}
+            C ${(s.y + d.y) / 2} ${s.x},
+              ${(s.y + d.y) / 2} ${d.x},
+              ${d.y} ${d.x}`;
+
+        return path;
+    }
+
+    click = (d) => {
+        if (d.children) {
+            d.childrens = d.children; // eslint-disable-line no-param-reassign
+            d.children = null; // eslint-disable-line no-param-reassign
+        } else {
+            d.children = d.childrens; // eslint-disable-line no-param-reassign
+            d.childrens = null; // eslint-disable-line no-param-reassign
+        }
+        this.duration = 500;
+        this.update(d);
+    }
+
+    addNodes = (group, source, nodes) => {
+        const { labelAccessor } = this.props;
+
+        let i = 0;
+        const node = group
+            .selectAll('g.node')
+            .data(nodes, (d) => {
+                if (d.id) {
+                    return d.id;
+                }
+                i += 1;
+                d.id = i; // eslint-disable-line no-param-reassign
+                return d.id;
+            });
+        const nodeEnter = node
+            .enter()
+            .append('g')
+            .attr('class', 'node')
+            .attr('transform', `translate(${source.y0}, ${source.x0})`)
+            .on('click', this.click);
+
+        nodeEnter
+            .append('circle')
+            .attr('class', 'node')
+            .attr('r', 0)
+            .style('stroke', '#fff')
+            .style('stroke-width', '2px')
+            .style('fill', this.topicColors);
+
+        nodeEnter
+            .append('text')
+            .attr('dy', '.35em')
+            .attr('fill-opacity', 0)
+            .attr('x', d => (d.children || d.childrens ? -13 : 13))
+            .attr('text-anchor', d => ((d.children || d.childrens) ? 'end' : 'start'))
+            .text(d => labelAccessor(d.data));
+
+        const nodeUpdate = nodeEnter.merge(node);
+
+        nodeUpdate
+            .transition()
+            .ease(easeSinInOut)
+            .duration(this.duration)
+            .attr('transform', d => `translate(${d.y}, ${d.x})`);
+
+        nodeUpdate
+            .select('circle.node')
+            .attr('r', 10)
+            .style('fill', this.topicColors)
+            .style('stroke', d => (d.childrens ? '#039be5' : '#fff'))
+            .attr('cursor', 'pointer');
+
+        nodeUpdate
+            .selectAll('text')
+            .transition()
+            .ease(easeSinInOut)
+            .style('fill-opacity', 1);
+
+        const nodeExit = node
+            .exit()
+            .transition()
+            .ease(easeSinInOut)
+            .duration(this.duration)
+            .attr('transform', `translate(${source.y}, ${source.x})`)
+            .remove();
+
+        nodeExit
+            .select('circle')
+            .attr('r', 0);
+
+        nodeExit
+            .select('text')
+            .style('fill-opacity', 0);
+    }
+
+    addLinks = (group, source, links) => {
+        const link = group
+            .selectAll('path.link')
+            .data(links, d => d.id);
+
+        const linkEnter = link
+            .enter()
+            .insert('path', 'g')
+            .attr('class', 'link')
+            .attr('stroke', this.topicColors)
+            .attr('stroke-width', '2px')
+            .attr('fill', 'none')
+            .attr('d', () => {
+                const out = { x: source.x0, y: source.y0 };
+                return this.diagonal(out, out);
+            });
+
+        const linkUpdate = linkEnter
+            .merge(link);
+
+        linkUpdate
+            .transition()
+            .ease(easeSinInOut)
+            .duration(this.duration)
+            .attr('d', d => this.diagonal(d, d.parent));
+
+        link
+            .exit()
+            .transition()
+            .ease(easeSinInOut)
+            .duration(this.duration)
+            .attr('d', () => {
+                const out = { x: source.x, y: source.y };
+                return this.diagonal(out, out);
+            })
+            .remove();
+    }
+
+    update = (source) => {
+        const treeData = this.trees(this.root);
+        const nodes = treeData.descendants();
+        const links = treeData.descendants().slice(1);
+
+        const group = this.context;
+
+        this.addNodes(group, source, nodes);
+        this.addLinks(group, source, links);
+        nodes.forEach((d) => {
+            d.x0 = d.x; // eslint-disable-line no-param-reassign
+            d.y0 = d.y; // eslint-disable-line no-param-reassign
+        });
+    }
+
+    drawChart = () => {
         const {
             data,
-            childrenAccessor,
             boundingClientRect,
-            labelAccessor,
-            colorScheme,
-            margins,
         } = this.props;
 
         if (!boundingClientRect.width) {
@@ -97,195 +293,29 @@ export default class CollapsibleTree extends React.PureComponent {
             return;
         }
 
-        let { width, height } = boundingClientRect;
-        const {
-            top,
-            right,
-            bottom,
-            left,
-        } = margins;
+        this.setupChart();
+        this.update(this.root);
+    }
 
-        const svg = select(this.svg);
-        svg.selectAll('*').remove();
-
-        width = width - left - right;
-        height = height - top - bottom;
-
-        const colors = scaleOrdinal()
-            .range(colorScheme);
-
-        function topicColors(node) {
-            let color = colors(0);
-            if (node.depth === 0 || node.depth === 1) {
-                color = colors(labelAccessor(node.data));
-            } else {
-                color = topicColors(node.parent);
-            }
-            return color;
-        }
-
-        function diagonal(s, d) {
-            const path = `M ${s.y} ${s.x}
-            C ${(s.y + d.y) / 2} ${s.x},
-              ${(s.y + d.y) / 2} ${d.x},
-              ${d.y} ${d.x}`;
-
-            return path;
-        }
-
-        const group = svg
-            .attr('width', width + left + right)
-            .attr('height', height + top + bottom)
-            .append('g')
-            .attr('transform', `translate(${left},${top})`);
-
-        const trees = tree()
-            .size([height, width - 160]);
-
-        const root = hierarchy(data, childrenAccessor);
-        root.x0 = height / 2;
-        root.y0 = 0;
-
-        let i = 0;
-        let transitionTime = 0;
-        function update(source) {
-            const treeData = trees(root);
-            const nodes = treeData.descendants();
-            const links = treeData.descendants().slice(1);
-
-            const node = group
-                .selectAll('g.node')
-                .data(nodes, (d) => {
-                    if (d.id) {
-                        return d.id;
-                    }
-                    d.id = ++i; //eslint-disable-line
-                    return d.id;
-                });
-
-            function click(d) {
-                if (d.children) {
-                    d.childrens = d.children; // eslint-disable-line
-                    d.children = null; // eslint-disable-line
-                } else {
-                    d.children = d.childrens; // eslint-disable-line
-                    d.childrens = null; // eslint-disable-line
-                }
-                transitionTime = 500;
-                update(d);
-            }
-
-            const nodeEnter = node
-                .enter()
-                .append('g')
-                .attr('class', 'node')
-                .attr('transform', `translate(${source.y0}, ${source.x0})`)
-                .on('click', click);
-
-            nodeEnter
-                .append('circle')
-                .attr('class', 'node')
-                .attr('r', 0)
-                .style('fill', topicColors);
-
-            nodeEnter
-                .append('text')
-                .attr('dy', '.35em')
-                .attr('fill-opacity', 0)
-                .attr('x', d => (d.children || d.childrens ? -13 : 13))
-                .attr('text-anchor', d => ((d.children || d.childrens) ? 'end' : 'start'))
-                .text(d => labelAccessor(d.data));
-
-            const nodeUpdate = nodeEnter.merge(node);
-
-            nodeUpdate
-                .transition()
-                .ease(easeSinInOut)
-                .duration(transitionTime)
-                .attr('transform', d => `translate(${d.y}, ${d.x})`);
-
-            nodeUpdate
-                .select('circle.node')
-                .attr('r', 10)
-                .style('fill', topicColors)
-                .style('stroke', d => (d.childrens ? '#039be5' : '#fff'))
-                .attr('cursor', 'pointer');
-
-            nodeUpdate
-                .selectAll('text')
-                .transition()
-                .ease(easeSinInOut)
-                .style('fill-opacity', 1);
-
-            const nodeExit = node
-                .exit()
-                .transition()
-                .ease(easeSinInOut)
-                .duration(transitionTime)
-                .attr('transform', `translate(${source.y}, ${source.x})`)
-                .remove();
-
-            nodeExit
-                .select('circle')
-                .attr('r', 0);
-
-            nodeExit
-                .select('text')
-                .style('fill-opacity', 0);
-
-            const link = group
-                .selectAll('path.link')
-                .data(links, d => d.id);
-
-            const linkEnter = link
-                .enter()
-                .insert('path', 'g')
-                .attr('class', 'link')
-                .attr('stroke', topicColors)
-                .attr('fill', 'none')
-                .attr('d', () => {
-                    const out = { x: source.x0, y: source.y0 };
-                    return diagonal(out, out);
-                });
-
-            const linkUpdate = linkEnter
-                .merge(link);
-
-            linkUpdate
-                .transition()
-                .ease(easeSinInOut)
-                .duration(transitionTime)
-                .attr('d', d => diagonal(d, d.parent));
-
-            link
-                .exit()
-                .transition()
-                .ease(easeSinInOut)
-                .duration(transitionTime)
-                .attr('d', () => {
-                    const out = { x: source.x, y: source.y };
-                    return diagonal(out, out);
-                })
-                .remove();
-
-            nodes.forEach((d) => {
-                d.x0  = d.x; // eslint-disable-line
-                d.y0 = d.y; // eslint-disable-line
-            });
-        }
-        update(root);
+    redrawChart = () => {
+        const context = select(this.svg);
+        context.selectAll('*').remove();
+        this.drawChart();
     }
 
     render() {
-        const { loading } = this.props;
+        const { loading, className } = this.props;
+
+        const containerStyle = `${styles['collapsible-tree-container']} ${className}`;
+        const collapsibleStyle = `${styles['collapsible-tree']}`;
 
         return (
             <div
-                className={`collapsible-tree-container ${this.props.className}`}
+                className={containerStyle}
             >
                 { loading && <LoadingAnimation /> }
                 <svg
-                    className="collapsible-tree"
+                    className={collapsibleStyle}
                     ref={(elem) => { this.svg = elem; }}
                 />
             </div>
