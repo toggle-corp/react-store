03fed2585993732f58c24caea9908f8c3ec124ca
frozenhelium <fren.ankit@gmail.com>
2018-05-06 15:47:29 +0545

    Fix import path

--- components/Input/HierarchicalMultiSelectInput.js
+++ components/Input/HierarchicalMultiSelectInput.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import MultiSelectInput from '../MultiSelectInput';
+import MultiSelectInput from './MultiSelectInput';
 
 const propTypes = {
     className: PropTypes.string,
