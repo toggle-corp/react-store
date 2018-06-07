a649a1a8597d2e1dedc256d48361c3e47ed1e099
Bibek Dahal <bibekdahal.bd16@gmail.com>
2018-06-07 11:22:18 +0545

    Fix some typescript definitions

--- components/View/List.d.ts
+++ components/View/List.d.ts
@@ -1,10 +1,19 @@
 import * as React from 'react';
 import { ExternalProps } from '../Input/Faram/FaramElement';
 
+interface RendererProps<T> {
+    className: string;
+    key: string | undefined;
+    datum: T;
+    index: number;
+    data: T[];
+}
+
 interface Props<T> {
     data?: T[];
     keyExtractor?(datum: T, index: number): string;
-    modifier?(key: string, dataum: T, i: number, data: T[]): React.ReactNode;
+    // FIXME: key should not be undefined
+    modifier?(key: string | undefined, dataum: T, i: number, data: T[]): React.ReactNode;
 }
 
 export type ListProps<T> = Props<T> & ExternalProps;
