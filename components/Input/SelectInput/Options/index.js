2ef38dc5188c2e7a31fcd02169b31ab96b989fab
frozenhelium <fren.ankit@gmail.com>
2018-06-27 12:21:19 +0545

    Fix select input not showing active option

--- components/Input/SelectInput/Options/index.js
+++ components/Input/SelectInput/Options/index.js
@@ -65,7 +65,7 @@ export default class Options extends React.PureComponent {
                 key={key}
                 optionKey={key}
                 onClick={onOptionClick}
-                active={isActive}
+                isActive={isActive}
             >
                 { label }
             </Option>
