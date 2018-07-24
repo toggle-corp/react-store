cc7fedd0254f5e11c54e9f8bfd39522473cd942a
Sameer Rai <sameerai2736@gmail.com>
2018-07-24 16:35:37 +0545

    Handle  mouse events

--- components/Visualization/SparkLines/index.js
+++ components/Visualization/SparkLines/index.js
@@ -1,11 +1,20 @@
 import React, {
     PureComponent,
 } from 'react';
-import { select } from 'd3-selection';
-import { extent } from 'd3-array';
+import {
+    select,
+    mouse,
+} from 'd3-selection';
+import {
+    extent,
+    bisector,
+} from 'd3-array';
 import { scaleLinear } from 'd3-scale';
 import { PropTypes } from 'prop-types';
-import { line, area } from 'd3-shape';
+import {
+    line,
+    area,
+} from 'd3-shape';
 import SvgSaver from 'svgsaver';
 import Responsive from '../../General/Responsive';
 
@@ -19,7 +28,11 @@ const propTypes = {
         height: PropTypes.number,
     }).isRequired,
     data: PropTypes.arrayOf(PropTypes.object),
-    valueAccessor: PropTypes.func.isRequired,
+    xValueAccessor: PropTypes.func.isRequired,
+    xValueModifier: PropTypes.func,
+    yValueAccessor: PropTypes.func.isRequired,
+    yValueModifier: PropTypes.func,
+    onHover: PropTypes.func,
     fill: PropTypes.bool,
     className: PropTypes.string,
     margins: PropTypes.shape({
@@ -32,13 +45,16 @@ const propTypes = {
 
 const defaultProps = {
     data: [],
-    fill: false,
+    fill: true,
+    onHover: () => {},
+    xValueModifier: d => d,
+    yValueModifier: d => d,
     className: '',
     margins: {
-        top: 0,
-        right: 0,
-        bottom: 0,
-        left: 0,
+        top: 5,
+        right: 5,
+        bottom: 5,
+        left: 5,
     },
 };
 
@@ -60,13 +76,38 @@ class SparkLines extends PureComponent {
         svgsaver.asSvg(svg.node(), `${getStandardFilename('sparklines', 'graph')}.svg`);
     }
 
+    handleMouseMove = (element, focus) => {
+        const {
+            data,
+            onHover,
+        } = this.props;
+        const {
+            scaleX,
+            scaleY,
+            xValue,
+            yValue,
+            bisectXValue,
+        } = this;
+        const x0 = scaleX.invert(mouse(element)[0]);
+        const i = bisectXValue(data, x0, 1);
+        const d0 = data[i - 1];
+        const d1 = data[i];
+        const d = x0 - xValue(d0) > xValue(d1) - x0 ? d1 : d0;
+        onHover(d);
+        focus
+            .attr('transform', `translate(${scaleX(xValue(d))}, ${scaleY(yValue(d))})`);
+    }
+
     drawChart = () => {
         const {
             data,
             boundingClientRect,
             margins,
             fill,
-            valueAccessor,
+            xValueAccessor,
+            xValueModifier,
+            yValueAccessor,
+            yValueModifier,
         } = this.props;
 
         if (!boundingClientRect.width || !data || data.length === 0) {
@@ -92,22 +133,28 @@ class SparkLines extends PureComponent {
             .attr('class', styles.sparkLine)
             .attr('transform', `translate(${left}, ${top})`);
 
-        const scaleX = scaleLinear()
-            .range([0, width])
-            .domain([0, data.length - 1]);
 
-        const scaleY = scaleLinear()
+        this.xValue = d => xValueModifier(xValueAccessor(d));
+        this.yValue = d => yValueModifier(yValueAccessor(d));
+
+        this.bisectXValue = bisector(this.xValue).left;
+
+        this.scaleX = scaleLinear()
+            .range([0, width])
+            .domain(extent(data.map(d => this.xValue(d))));
+
+        this.scaleY = scaleLinear()
             .range([height, 0])
-            .domain(extent(data.map(d => valueAccessor(d))));
+            .domain(extent(data.map(d => this.yValue(d))));
 
         const areas = area()
-            .x((d, i) => scaleX(i))
+            .x(d => this.scaleX(this.xValue(d)))
             .y0(height)
-            .y1(d => scaleY(valueAccessor(d)));
+            .y1(d => this.scaleY(this.yValue(d)));
 
         const lines = line()
-            .x((d, i) => scaleX(i))
-            .y(d => scaleY(valueAccessor(d)));
+            .x(d => this.scaleX(this.xValue(d)))
+            .y(d => this.scaleY(this.yValue(d)));
 
         if (fill) {
             group.append('path')
@@ -122,6 +169,27 @@ class SparkLines extends PureComponent {
             .datum(data)
             .attr('d', lines)
             .style('fill', 'none');
+
+        const focus = group
+            .append('g')
+            .attr('class', styles.focus)
+            .style('display', 'none');
+
+        focus
+            .append('circle')
+            .attr('r', 5);
+
+        group
+            .append('rect')
+            .attr('class', 'overlay')
+            .style('fill', 'none')
+            .style('pointer-events', 'all')
+            .attr('width', width + left + right)
+            .attr('height', height + top + bottom)
+            .attr('transform', `translate(${left}, ${top})`)
+            .on('mouseover', () => focus.style('display', null))
+            .on('mouseout', () => focus.style('display', 'none'))
+            .on('mousemove', (d, i, nodes) => this.handleMouseMove(nodes[0], focus));
     }
 
     redrawChart = () => {
