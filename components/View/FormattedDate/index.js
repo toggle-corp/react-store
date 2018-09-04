dba6ef8e00f8a80cfcf476e62db002b875d79507
tnagorra <weathermist@gmail.com>
2018-09-04 13:35:21 +0545

    Add 'value' props for FormattedDate; Add FormattedTime component

--- components/View/FormattedDate/index.js
+++ components/View/FormattedDate/index.js
@@ -1,1 +1,33 @@
-export { default } from './FormattedDate.js';
+import PropTypes from 'prop-types';
+import React from 'react';
+
+import FormattedDate from './FormattedDate';
+import { isTruthy } from '../../../utils/common';
+
+const HackedFormattedDate = ({ value, ...otherProps }) => {
+    if (isTruthy(value)) {
+        return (
+            <FormattedDate
+                {...otherProps}
+                date={value}
+            />
+        );
+    }
+    return (
+        <FormattedDate
+            {...otherProps}
+        />
+    );
+};
+
+HackedFormattedDate.propTypes = {
+    value: PropTypes.oneOfType([
+        PropTypes.number,
+        PropTypes.string,
+    ]),
+};
+HackedFormattedDate.defaultProps = {
+    value: undefined,
+};
+
+export default HackedFormattedDate;
