a03881d94bd6395ede450eb1250ef4e98bd909ee
frozenhelium <fren.ankit@gmail.com>
2019-02-15 14:43:34 +0545

    Improve word cloud

--- components/Visualization/WordCloud/index.js
+++ components/Visualization/WordCloud/index.js
@@ -5,6 +5,8 @@ import SvgSaver from 'svgsaver';
 import { select } from 'd3-selection';
 import { scaleOrdinal } from 'd3-scale';
 import { schemeSet1 } from 'd3-scale-chromatic';
+import { _cs } from '@togglecorp/fujs';
+import memoize from 'memoize-one';
 
 import Responsive from '../../General/Responsive';
 import { getStandardFilename } from '../../../utils/common';
@@ -17,33 +19,31 @@ const propTypes = {
         width: PropTypes.number,
         height: PropTypes.number,
     }).isRequired,
-    data: PropTypes.arrayOf(
-        PropTypes.shape({
-            text: PropTypes.string,
-            size: PropTypes.number,
-        }),
-    ).isRequired,
-    padding: PropTypes.oneOfType([PropTypes.number, PropTypes.func]),
+    data: PropTypes.arrayOf(PropTypes.object).isRequired,
     font: PropTypes.oneOfType([PropTypes.string, PropTypes.func]),
     rotate: PropTypes.oneOfType([PropTypes.number, PropTypes.func]),
     setSaveFunction: PropTypes.func,
+    textSelector: PropTypes.func,
     fontSizeSelector: PropTypes.func,
-    onWordMouseOver: PropTypes.func,
-    onWordClick: PropTypes.func,
+    // onWordMouseOver: PropTypes.func,
+    // onWordClick: PropTypes.func,
     colorScheme: PropTypes.arrayOf(PropTypes.string),
 };
 
 const defaultProps = {
     className: '',
-    font: 'serif',
-    padding: 5,
+    font: 'sans-serif',
     rotate: 0,
     setSaveFunction: undefined,
     onWordClick: undefined,
     onWordMouseOver: undefined,
+    textSelector: d => d.text,
     fontSizeSelector: d => d.size,
     colorScheme: schemeSet1,
 };
+
+const emptyObject = {};
+const emptyList = [];
 
 class WordCloud extends PureComponent {
     static propTypes = propTypes;
@@ -51,106 +51,180 @@ class WordCloud extends PureComponent {
 
     constructor(props) {
         super(props);
+
         if (props.setSaveFunction) {
             props.setSaveFunction(this.save);
         }
+
+        this.state = {
+            calculatedWords: emptyList,
+        };
+
+        this.startWordCloudComputation(props);
     }
 
-    componentDidMount() {
-        this.drawChart();
+    componentWillReceiveProps(nextProps) {
+        this.startWordCloudComputation(nextProps);
     }
 
-    componentDidUpdate() {
-        this.redrawChart();
+    getColorScale = colorScheme => scaleOrdinal().range(colorScheme);
+
+    startWordCloudComputation = ({
+        boundingClientRect: {
+            width: containerWidth,
+            height: containerHeight,
+        } = emptyObject,
+        data,
+        font,
+        rotate,
+        textSelector,
+        fontSizeSelector,
+    }) => {
+        const isContainerInvalid = !containerWidth;
+        const isDataInvalid = !data || data.length === 0;
+
+        if (isContainerInvalid || isDataInvalid) {
+            return;
+        }
+
+        this.calculateWordCloud(
+            containerWidth,
+            containerHeight,
+            font,
+            data,
+            rotate,
+            textSelector,
+            fontSizeSelector,
+        );
     }
 
     save = () => {
+        const { current: svgElement } = this.svgRef;
+        const svg = select(svgElement);
         const svgsaver = new SvgSaver();
-        const svg = select(this.svg);
         svgsaver.asSvg(svg.node(), `${getStandardFilename('wordcloud', 'graph')}.svg`);
     }
 
-    redrawChart = () => {
-        const svg = select(this.svg);
-        svg.selectAll('*').remove();
-        this.drawChart();
+    handleWordCloudCalculationEnd = (words) => {
+        this.setState({ calculatedWords: words });
     }
 
-    drawChart = () => {
+    calculateWordCloud = memoize((
+        width,
+        height,
+        font,
+        data,
+        rotate,
+        textSelector,
+        sizeSelector,
+    ) => {
+        this.setState({ calculatedWords: undefined });
+        const renderArea = width * height;
+
+        const words = data.map(d => textSelector(d));
+        const frequencies = data.map(d => sizeSelector(d));
+
+        const totalLetterLength = words.join(' ').length;
+
+        const minFont = 3;
+        const maxFont = Math.max(minFont * 2, Math.sqrt(renderArea / totalLetterLength));
+        const maxSize = Math.max(Math.max(...frequencies), 1);
+
+        const renderAreaFactor = renderArea / totalLetterLength;
+        const paddingFactor = 5;
+        const padding = paddingFactor > 0 ? Math.sqrt(renderAreaFactor) / paddingFactor : 0;
+
+        const sizeOffset = maxFont / maxSize;
+
+        const fontSizeSelector = d => Math.max(3, Math.min(d.size * sizeOffset, maxFont));
+
+        const layout = cloud();
+        const layoutData = JSON.parse(JSON.stringify(data));
+        layout.size([width, height])
+            .font(font)
+            .words(layoutData)
+            .padding(padding)
+            .rotate(rotate)
+            .fontSize(fontSizeSelector)
+            .text(textSelector)
+            .on('end', this.handleWordCloudCalculationEnd);
+
+        setTimeout(layout.start, 0);
+    })
+
+    render() {
         const {
+            className: classNameFromProps,
+            boundingClientRect: {
+                width: containerWidth,
+                height: containerHeight,
+            } = emptyObject,
             data,
-            boundingClientRect,
+            colorScheme,
             font,
             padding,
             rotate,
             fontSizeSelector,
-            onWordClick,
-            onWordMouseOver,
-            colorScheme,
+            textSelector,
         } = this.props;
 
-        if (!boundingClientRect.width || !data || data.length === 0) {
-            return;
+        const isContainerInvalid = !containerWidth;
+        const isDataInvalid = !data || data.length === 0;
+
+        if (isContainerInvalid || isDataInvalid) {
+            return null;
         }
 
-        const {
-            width,
-            height,
-        } = boundingClientRect;
-
-        const colors = scaleOrdinal().range(colorScheme);
-
-        const layout = cloud()
-            .size([width, height])
-            .font(font)
-            .words(data)
-            .padding(padding)
-            .rotate(rotate)
-            .fontSize(fontSizeSelector)
-            .on('end', (words) => {
-                const group = select(this.svg)
-                    .append('g')
-                    .attr('transform', `translate(${layout.size()[0] / 2}, ${layout.size()[1] / 2})`);
-
-
-                const texts = group
-                    .selectAll('text')
-                    .data(words)
-                    .enter()
-                    .append('text')
-                    .style('font-size', d => `${d.size}px`)
-                    .style('font-family', font)
-                    .style('fill', (d, i) => colors(i))
-                    .attr('text-anchor', 'middle')
-                    .attr('transform', d => `translate(${d.x}, ${d.y}) rotate(${d.rotate})`)
-                    .text(d => d.text);
-
-                if (onWordClick) {
-                    texts.on('click', onWordClick);
-                }
-
-                if (onWordMouseOver) {
-                    texts.on('mouseover', onWordMouseOver);
-                }
-            });
-
-        layout.start();
-    }
-
-    render() {
-        const { className: classNameFromProps } = this.props;
-
-        const className = [
-            'word-cloud',
-            styles.wordCloud,
+        const className = _cs(
             classNameFromProps,
-        ].join(' ');
+            styles.wordCloud,
+            'word-cloud',
+        );
+
+        /*
+        const svgClassName = _cs(
+            'svg',
+            styles.svg,
+        );
+        */
+
+        const colorScale = this.getColorScale(colorScheme);
+        const { calculatedWords } = this.state;
 
         return (
-            <svg
+            <div
                 className={className}
-                ref={(elem) => { this.svg = elem; }}
-            />
+                style={{
+                    width: containerWidth,
+                    height: containerHeight,
+                }}
+            >
+                { calculatedWords && (
+                    <svg
+                        // className={svgClassName}
+                        width={containerWidth}
+                        height={containerHeight}
+                        ref={this.svgRef}
+                    >
+                        <g transform={`translate(${containerWidth / 2}, ${containerHeight / 2})`}>
+                            { calculatedWords.map(w => (
+                                <text
+                                    key={w.text}
+                                    style={{
+                                        fontSize: w.size,
+                                        fontFamily: w.font,
+                                    }}
+                                    fill={colorScale(w.text)}
+                                    textAnchor="middle"
+                                    transform={`translate(${w.x}, ${w.y}) rotate(${w.rotate})`}
+                                >
+                                    { w.text }
+                                </text>
+                            )) }
+                        </g>
+                    </svg>
+                ) }
+            </div>
         );
     }
 }
