974dd10c081f6b2241f31a2eb62c3fd3ace62c39
frozenhelium <fren.ankit@gmail.com>
2019-02-24 12:22:12 +0545

    Use css vars in colors and dimens

---
+++ utils/styles/default/colors.js
@@ -0,0 +1,137 @@
+/*
+@include background-color($color-primary);
+
+background-color: $color-primary;
+color: $color-text-primary;
+*/
+
+const colorTextOnLight = 'rgba(0, 0, 0, .7)';
+const colorTextOnDark = 'rgba(255, 255, 255, .8)';
+
+const colorText = 'rgba(0, 0, 0, .7)';
+const colorTextAlt = '#000';
+
+const colorTextPlaceholder = 'rgba(0, 0, 0, .4)';
+const colorTextDisabled = 'rgba(0, 0, 0, .3)';
+const colorTextLabel = 'rgba(0, 0, 0, .6)';
+
+const colorPrimary = '#008975';
+const colorTextOnPrimary = colorTextOnDark;
+const colorAccent = '#c2185b';
+const colorTextOnAccent = colorTextOnDark;
+const colorInfo = '#41a1f0';
+const colorTextOnInfo = colorTextOnDark;
+const colorSuccess = '#41cf76';
+const colorTextOnSuccess = colorTextOnDark;
+const colorWarning = '#e0b652';
+const colorTextOnWarning = colorTextOnDark;
+const colorDanger = '#d3575e';
+const colorTextOnDanger = colorTextOnDark;
+
+const colorBackground = '#e0e0e0';
+const colorBackgroundAlt = '#f0f0f0';
+
+const colorForeground = '#fff';
+const colorForegroundAlt = '#f5f5f5';
+
+const colorBackgroundHeader = '#bbdefb';
+const colorBackgroundHeaderAlt = '#90caf9';
+
+const colorBackgroundHover = 'rgba(0, 0, 0, .03)';
+const colorBackgroundDisabled = 'rgba(0, 0, 0, .05)';
+
+const colorBackgroundSelected = colorAccent;
+
+const colorBackgroundRow = '#fafafa';
+const colorBackgroundRowAlt = '#f0f0f0';
+
+const colorNavbar = '#fff';
+const colorTextOnNavbar = colorTextOnLight;
+const colorTextActiveTab = colorPrimary;
+
+const colorNegative = colorDanger;
+const colorPositive = colorSuccess;
+
+const colorSeparator = 'rgba(0, 0, 0, .1)';
+const colorSeparatorAlt = 'rgba(0, 0, 0, .2)';
+const colorSeparatorHover = 'rgba(0, 0, 0, .3)';
+
+const colorShadowMedium = 'rgba(0, 0, 0, .5)';
+const colorBackgroundButtonDefault = colorForeground;
+const colorBackgroundScrollbar = '#eee';
+const colorForegroundScrollbar = '#bbb';
+const colorBackgroundOverlay = 'rgba(255, 255, 255, .77)';
+const colorBackgroundModal = 'rgba(0, 0, 0, .77)';
+
+const colorGridPath = '#e8e8e8';
+const colorAxisPath = '#d3d3d3';
+const colorLinks = '#eaeaea';
+const colorVizBlue = '#3182bd';
+const colorVizGrey = '#d3d3d3';
+const colorVizOrange = '#FA6900';
+
+const colors = {
+    colorText,
+    colorTextPlaceholder,
+    colorTextDisabled,
+    colorTextLabel,
+
+    colorPrimary,
+    colorTextOnPrimary,
+    colorAccent,
+    colorTextOnAccent,
+    colorInfo,
+    colorTextOnInfo,
+    colorSuccess,
+    colorTextOnSuccess,
+    colorWarning,
+    colorTextOnWarning,
+    colorDanger,
+    colorTextOnDanger,
+
+    colorBackground,
+    colorBackgroundAlt,
+
+    colorForeground,
+    colorForegroundAlt,
+
+    colorBackgroundHeader,
+    colorBackgroundHeaderAlt,
+
+    colorBackgroundHover,
+    colorBackgroundDisabled,
+
+    colorBackgroundSelected,
+
+    colorBackgroundRow,
+    colorBackgroundRowAlt,
+
+    colorNavbar,
+    colorTextOnNavbar,
+    colorTextActiveTab,
+
+    colorNegative,
+    colorPositive,
+
+    colorSeparator,
+    colorSeparatorAlt,
+    colorSeparatorHover,
+
+    colorShadowMedium,
+    colorBackgroundButtonDefault,
+    colorBackgroundScrollbar,
+    colorForegroundScrollbar,
+    colorBackgroundOverlay,
+    colorBackgroundModal,
+
+    colorGridPath,
+    colorAxisPath,
+    colorLinks,
+    colorVizBlue,
+    colorVizGrey,
+    colorVizOrange,
+
+    colorTextAlt,
+};
+
+export default colors;
