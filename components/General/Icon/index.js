da7d4e6b8d970a715fa1632c3df8a87f65db7614
frozenhelium <fren.ankit@gmail.com>
2019-03-11 00:05:20 +0545

    Extend props in Icon component

--- components/General/Icon/index.js
+++ components/General/Icon/index.js
@@ -21,6 +21,7 @@ Object.keys(iconNames).forEach((key) => {
 const Icon = ({
     className,
     name,
+    ...otherProps
 }) => {
     const icon = iconMapping[name];
     if (!icon) {
@@ -30,11 +31,15 @@ const Icon = ({
     switch (icon.type) {
         case 'font':
             return (
-                <span className={_cs(className, icon.value)} />
+                <span
+                    {...otherProps}
+                    className={_cs(className, icon.value)}
+                />
             );
         case 'svg':
             return (
                 <ScalableVectorGraphics
+                    {...otherProps}
                     className={_cs(className, styles.svg)}
                     src={icon.value}
                 />
@@ -43,6 +48,7 @@ const Icon = ({
             return (
                 <img
                     alt={icon.altText}
+                    {...otherProps}
                     className={_cs(className, styles.image)}
                     src={icon.value}
                 />
