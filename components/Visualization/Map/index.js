c647c1421cf5e7e9751bd1f52980fd60045d54fd
tnagorra <weathermist@gmail.com>
2019-03-27 10:07:21 +0545

    Change api for map

--- components/Visualization/Map/index.js
+++ components/Visualization/Map/index.js
@@ -44,7 +44,7 @@ const propTypes = {
     navControlPosition: PropTypes.string,
     geoControlPosition: PropTypes.string,
     scaleControlPosition: PropTypes.string,
-    geoOnStartup: PropTypes.bool,
+    locateOnStartup: PropTypes.bool,
     showNavControl: PropTypes.bool,
     showGeolocationControl: PropTypes.bool,
     showScaleControl: PropTypes.bool,
@@ -72,7 +72,7 @@ const defaultProps = {
     showNavControl: false,
     showGeolocationControl: false,
     showScaleControl: false,
-    geoOnStartup: false,
+    locateOnStartup: false,
 
     mapStyle: DEFAULT_STYLE,
     zoom: DEFAULT_ZOOM_LEVEL,
@@ -116,10 +116,11 @@ export default class Map extends React.PureComponent {
             navControlPosition,
             showNavControl,
 
-            geoOnStartup,
+            locateOnStartup,
             geoOptions,
             geoControlPosition,
             showGeolocationControl,
+            onGeolocationChange,
 
             scaleOptions,
             scaleControlPosition,
@@ -166,9 +167,10 @@ export default class Map extends React.PureComponent {
                 geolocate,
                 geoControlPosition,
             );
+            geolocate.on('geolocate', onGeolocationChange);
         }
 
-        map.on('load', () => this.handleLoad(map, geoOnStartup, geolocate));
+        map.on('load', () => this.handleLoad(map, locateOnStartup, geolocate));
         map.on('zoom', () => this.handleZoomChange(map));
         map.on(
             'style.load',
@@ -245,7 +247,7 @@ export default class Map extends React.PureComponent {
         // this.sourceDestroyers = {};
     }
 
-    handleLoad = (map, geoOnStartup, geolocate) => {
+    handleLoad = (map, locateOnStartup, geolocate) => {
         // Since the map is loaded asynchronously, make sure
         // we are still mounted before doing setState
         if (!this.mounted) {
@@ -266,7 +268,7 @@ export default class Map extends React.PureComponent {
             zoomLevel: zoom,
         });
 
-        if (geolocate && geoOnStartup) {
+        if (geolocate && locateOnStartup) {
             geolocate.trigger();
         }
     }
