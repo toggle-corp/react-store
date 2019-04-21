7878d51d517f7bc3ee1f444c1d78957a7c50eede
tnagorra <weathermist@gmail.com>
2019-04-22 02:23:15 +0545

    Only show items with value greater than zero

--- components/Visualization/Legend/index.js
+++ components/Visualization/Legend/index.js
@@ -1,21 +1,29 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import memoize from 'memoize-one';
 
 import ListView from '#rscv/List/ListView';
 
 import LegendItem from './LegendItem';
-import styles from './styles.scss';
+// import styles from './styles.scss';
 
 const propTypes = {
     className: PropTypes.string,
     itemClassName: PropTypes.string,
     iconSelector: PropTypes.func,
+    keySelector: PropTypes.func.isRequired,
+    labelSelector: PropTypes.func.isRequired,
+    colorSelector: PropTypes.func.isRequired,
+    valueSelector: PropTypes.func,
+    data: PropTypes.array, // eslint-disable-next-line react/forbid-prop-types
 };
 
 const defaultProps = {
     className: '',
     itemClassName: '',
     iconSelector: () => undefined,
+    valueSelector: undefined,
+    data: [],
 };
 
 export default class Legend extends React.PureComponent {
@@ -38,17 +46,26 @@ export default class Legend extends React.PureComponent {
         });
     }
 
+    filterData = memoize((data, valueSelector) => (
+        valueSelector
+            ? data.filter(val => valueSelector(val) > 0)
+            : data
+    ))
+
     render() {
         const {
             data,
             className,
             keySelector,
             emptyComponent,
+            valueSelector,
         } = this.props;
+
+        const filteredData = this.filterData(data, valueSelector);
 
         return (
             <ListView
-                data={data}
+                data={filteredData}
                 className={className}
                 renderer={LegendItem}
                 keySelector={keySelector}
