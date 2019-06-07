d68dc1aa1b20166a90852ce6f6b8657858fec2da
Aditya Khatri <adityakhatri47@gmail.com>
2019-06-07 08:56:50 +0545

    Use react-word-cloud library for NewWordCloud

---
+++ components/Visualization/ReactWordCloud/index.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import memoize from 'memoize-one';
+import hoistNonReactStatics from 'hoist-non-react-statics';
+import ReactWordCloud from 'react-wordcloud';
+
+import Responsive from '../../General/Responsive';
+
+const SizeWrapper = (WrappedComponent) => {
+    // eslint-disable-next-line react/prefer-stateless-function
+    class ResponsiveElement extends React.Component {
+        static propTypes = {
+            // eslint-disable-next-line react/forbid-prop-types
+            boundingClientRect: PropTypes.object.isRequired,
+            className: PropTypes.string.isRequired,
+        };
+
+        static defaultProps = {
+            className: '',
+        };
+
+        getSize = memoize((width, height) => ([width, height]));
+
+        render() {
+            const {
+                className,
+                boundingClientRect: {
+                    width = [],
+                    height = [],
+                },
+                ...otherProps
+            } = this.props;
+
+            const size = this.getSize(width, height);
+
+            return (
+                <WrappedComponent
+                    className={className}
+                    minSize={size}
+                    size={size}
+                    {...otherProps}
+                />
+            );
+        }
+    }
+
+    return hoistNonReactStatics(
+        ResponsiveElement,
+        WrappedComponent,
+    );
+};
+
+const WordCloud = Responsive(SizeWrapper(ReactWordCloud));
+
+export default WordCloud;
