4a890d05f69aae42785d185df859c88970448f2b
tnagorra <weathermist@gmail.com>
2021-04-29 19:44:50 +0545

    Fix typing issues

--- components/General/Modalize.d.ts
+++ components/General/Modalize.d.ts
@@ -2,7 +2,7 @@ import * as React from 'react';
 
 // eslint-disable-next-line @typescript-eslint/ban-types
 declare function modalize<T extends object>(
-    component: (props: T) => JSX.Element,
+    component: React.ReactComponent<T>,
 ): React.ReactComponent<T & { modal: React.ReactElement; initialShowModal?: boolean }>;
 
 export default modalize;
